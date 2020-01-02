4415f95419c3eb40c92429a3156854fccb1689ad
Clechay <luke.clechay@gmail.com>
2020-01-02 18:55:00 +0100

    up

---
+++ README.md
@@ -0,0 +1,3 @@
+1. uruchomić przykład
+2. zrobić prostą kartkę z pokedexu
+3. pozwolić wybrać pokemona do wyświetlenia
--- scripts.js
+++ scripts.js
@@ -2,4 +2,4 @@
 	const request = await fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
 	const response = await request.json();
 	console.log(response);
-  })()
+})()
